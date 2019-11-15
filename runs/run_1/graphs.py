bf5671f36ec96f4114d0c3cc46d6e6cef3d166e5
williamstravis <travis.williams@nrel.gov>
2019-11-14 17:01:09 -0700

    automating profile graphing

--- runs/run_1/graphs.py
+++ runs/run_1/graphs.py
@@ -25,13 +25,13 @@ files = ["pvwattsv5_fixed_2016.h5", "pvwattsv5_tracking_2016.h5",
          "pvwattsv5_fixed_2017.h5", "pvwattsv5_tracking_2017.h5"]
 
 # 2016 fixed pv
-sample = files[0]
+sample = files[1]
 
 # Get year
 year = int(sample[sample.index(".") - 4: sample.index(".")])
 
 # Open file
-file = hp.File(files[0], mode="r")
+file = hp.File(sample, mode="r")
 
 # Get keys?
 keys = file.keys()
@@ -46,11 +46,29 @@ time = file["time_index"]
 t1 = dt.datetime(year, 1, 1, 0, 0).strftime("%Y-%m-%d %H:%M")
 t2 = dt.datetime(year, 12, 31, 23, 59).strftime("%Y-%m-%d %H:%M")
 dates = pd.date_range(t1, t2, freq="30 min")
-
-
+dates = [dt.datetime.strftime(d, "%Y-%m-%d %H:%M") for d in dates]
 
 # Capacity Factor Means
 plt.hist(cf_mean, bins=50)
 
-
 # Capacity Factor Profile
+cf = cf_profile.T
+poa = poa.T
+extent = 0, cf.shape[1], 0, cf.shape[0]
+
+fig, axes = plt.subplots(2, 1, figsize=(15, 4),  )
+ax1 = axes[0]
+ax1.imshow(cf)
+ax1.set_aspect('auto')
+ax1.set_xticks([])
+ax1.set_title("Capacity Factor")
+
+ax2 = axes[1]
+ax2.imshow(poa)
+ax2.set_aspect('auto')
+#ax2.set_xticklabels(dates)
+ax2.set_title("POA")
+fig.suptitle('Denver Area Solar Profiles - PV E-W Tracking (2016)', y=1.08,
+             fontsize=20)
+fig.tight_layout()
+fig.autofmt_xdate()
