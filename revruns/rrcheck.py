6d83891de098557c3b14d528010bbeefab5fa8f4
williamstravis <travis.williams@nrel.gov>
2019-12-05 13:43:20 -0700

    fixed up rrcheck to account for one dimensional datasets

--- revruns/rrcheck.py
+++ revruns/rrcheck.py
@@ -10,8 +10,10 @@ Created on Mon Nov 25 08:52:00 2019
 @author: twillia2
 """
 import click
+import h5py
 import json
 import multiprocessing as mp
+import numpy as np
 import os
 import pandas as pd
 import sys
@@ -26,17 +28,33 @@ SAVE_HELP = ("The path to use for the csv output file (defaults to " +
              "'./checkvars.csv').")
 
 def single_info(file):
-    """Return summary statistics of all data sets in a single hdf5 file"""
+    """Return summary statistics of all data sets in a single hdf5 file.
 
-    # Open the file
+    So, GDAL handles the multi-dimesional data sets fairly quickly, but doesn't
+    even detect the singular dimensional data sets. Actually, it doesn't always
+    detect the multidimensional datasets!
+
+    For the 1-D data sets, we could probably just use 5py, but definitely not
+    for the 2-D ones. However, if it is inconsistently detecting even these,
+    what to do?
+
+    First, see if this has happened to anyone else...
+
+    """
+    # GDAL For multidimensional data sets
     pointer = gdal.Open(file)
 
     # Get the list of sub data sets in each file
     subds = pointer.GetSubDatasets()
 
+    # If there was only one detectable data set, we'll need to use this instead
+    if len(subds) == 0:
+        subds = [(pointer.GetDescription(),)]
+
     # For each of these sub data sets, get an info dictionary
     stat_dicts = []
     for sub in subds:
+
         # Turn off most options to try and speed this up
         info_str = gdal.Info(sub[0],
                              stats=True,
@@ -58,16 +76,15 @@ def single_info(file):
 
         # Read this as a dictionary
         info = json.loads(info_str)
-        filename = info["files"][0]
         desc = info["description"]
-        var = desc[desc.index("//") + 2: ]
+        ds = desc[desc.index("//") + 2: ]  # data set name
         stats = info["bands"][0]
-        max_threshold = VARIABLE_CHECKS[var][1]
-        min_threshold = VARIABLE_CHECKS[var][0]
+        max_threshold = VARIABLE_CHECKS[ds][1]
+        min_threshold = VARIABLE_CHECKS[ds][0]
 
         # Return just these elements
-        stat_dict = {"file": filename,
-                     "data_set": var,
+        stat_dict = {"file": file,
+                     "data_set": ds,
                      "min": stats["minimum"],
                      "max": stats["maximum"],
                      "mean": stats["mean"],
@@ -76,7 +93,47 @@ def single_info(file):
                      "max_threshold": max_threshold}
         stat_dicts.append(stat_dict)
 
-    return pd.DataFrame(stat_dicts)
+    # To better account for completed data sets, make a data frame
+    gdal_data = pd.DataFrame(stat_dicts)
+
+    # H5py for one-dimensional data sets
+    with h5py.File(file) as data_set:
+        keys = data_set.keys()
+        keys = [k for k in keys if k not in gdal_data["data_set"].values]
+        keys = [k for k in keys if k not in ["meta", "time_index"]]
+        data_sets = {k: data_set[k][:] for k in keys}
+
+    # Now we have to calculate these "manually" (lol)
+    stat_dicts = []
+    for ds in data_sets.keys():
+        values = data_sets[ds]
+        if ds in VARIABLE_CHECKS:
+            max_threshold = VARIABLE_CHECKS[ds][1]  # Figure out the thresholds for these
+            min_threshold = VARIABLE_CHECKS[ds][0]
+        else:
+            max_threshold = np.nan
+            min_threshold = np.nan
+        minv = np.min(values)
+        maxv = np.max(values)
+        meanv = np.mean(values)
+        stdv = np.std(values)
+        stat_dict = {"file": file,
+                     "data_set": ds,
+                     "min": minv,
+                     "max": maxv,
+                     "mean": meanv,
+                     "std": stdv,
+                     "min_threshold": min_threshold,
+                     "max_threshold": max_threshold}
+        stat_dicts.append(stat_dict)
+
+    # Make another data frame with the 1-D data set statistics
+    hdf_data = pd.DataFrame(stat_dicts)
+
+    # Concatenate the two together
+    summary_table = pd.concat([gdal_data, hdf_data]).reset_index(drop=True)
+
+    return summary_table
 
 
 # The command
