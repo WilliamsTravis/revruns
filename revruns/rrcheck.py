1c352e401c360573bba3a7f27be577901f8b256a
Travis Williams <twillia2@el1.ib0.cm.hpc.nrel.gov>
2019-12-09 15:26:51 -0700

    adjusted rrcheck to flag broken or questionably healthy files

--- revruns/rrcheck.py
+++ revruns/rrcheck.py
@@ -44,64 +44,74 @@ def gdal_info(file):
     # GDAL For multidimensional data sets
     try:
         pointer = gdal.Open(file)
-    except RuntimeError as e:
-        # We could handle this some how
-        print("Broken file")
-        raise
+        health = "good"
+        # Get the list of sub data sets in each file
+        subds = pointer.GetSubDatasets()
 
-    # Get the list of sub data sets in each file
-    subds = pointer.GetSubDatasets()
+        # If there was only one detectable data set, we'll need to use this instead
+        if len(subds) == 0:
+            subds = [(pointer.GetDescription(),)]
+            assert "HDF5" in subds[0][0]
 
-    # If there was only one detectable data set, we'll need to use this instead
-    if len(subds) == 0:
-        subds = [(pointer.GetDescription(),)]
+    except RuntimeError:
+        print("Broken file: " + os.path.basename(file))
+        health = "broken"
 
-    # For each of these sub data sets, get an info dictionary
-    stat_dicts = []
-    for sub in subds:
+    except AssertionError:
+        health = "questionable"
 
-        # Turn off most options to try and speed this up
-        info_str = gdal.Info(sub[0],
-                             stats=True,
-                             showFileList=True,
-                             format="json",
-                             listMDD=False,
-                             approxStats=False,
-                             deserialize=False,
-                             computeMinMax=False,
-                             reportHistograms=False,
-                             reportProj4=False,
-                             computeChecksum=False,
-                             showGCPs=False,
-                             showMetadata=False,
-                             showRAT=False,
-                             showColorTable=False,
-                             allMetadata=False,
-                             extraMDDomains=None)
+    # If this opens
+    if health == "good":
+        # For each of these sub data sets, get an info dictionary
+        stat_dicts = []
+        for sub in subds:
 
-        # Read this as a dictionary
-        info = json.loads(info_str)
-        desc = info["description"]
-        ds = desc[desc.index("//") + 2: ]  # data set name
-        stats = info["bands"][0]
-        max_threshold = VARIABLE_CHECKS[ds][1]
-        min_threshold = VARIABLE_CHECKS[ds][0]
+            # Turn off most options to try and speed this up
+            info_str = gdal.Info(sub[0],
+                                 stats=True,
+                                 showFileList=True,
+                                 format="json",
+                                 listMDD=False,
+                                 approxStats=False,
+                                 deserialize=False,
+                                 computeMinMax=False,
+                                 reportHistograms=False,
+                                 reportProj4=False,
+                                 computeChecksum=False,
+                                 showGCPs=False,
+                                 showMetadata=False,
+                                 showRAT=False,
+                                 showColorTable=False,
+                                 allMetadata=False,
+                                 extraMDDomains=None)
 
-        # Return just these elements
-        stat_dict = {"file": file,
-                     "data_set": ds,
-                     "min": stats["minimum"],
-                     "max": stats["maximum"],
-                     "mean": stats["mean"],
-                     "std": stats["stdDev"],
-                     "min_threshold": min_threshold,
-                     "max_threshold": max_threshold}
-        stat_dicts.append(stat_dict)
+            # Read this as a dictionary
+            info = json.loads(info_str)
+            desc = info["description"]
+            ds = desc[desc.index("//") + 2: ]  # data set name
+            stats = info["bands"][0]
+            max_threshold = VARIABLE_CHECKS[ds][1]
+            min_threshold = VARIABLE_CHECKS[ds][0]
 
-    # To better account for completed data sets, make a data frame
-    gdal_data = pd.DataFrame(stat_dicts)
+            # Return just these elements
+            stat_dict = {"file": file,
+                         "data_set": ds,
+                         "min": stats["minimum"],
+                         "max": stats["maximum"],
+                         "mean": stats["mean"],
+                         "std": stats["stdDev"],
+                         "min_threshold": min_threshold,
+                         "max_threshold": max_threshold}
+            stat_dicts.append(stat_dict)
 
-    return gdal_data
+        # To better account for completed data sets, make a data frame
+        gdal_data = pd.DataFrame(stat_dicts)
+
+    else:
+        gdal_data = pd.DataFrame(columns = ["file", "data_set", "min", "max", "mean", "std", "min_threshold",
+                                            "max_threshold"])
+
+    return gdal_data, health
 
 
 def single_info(file):
@@ -115,88 +125,89 @@ def single_info(file):
     gdal_data, health = gdal_info(file)
 
     # It might be empty
-    if gdal_data.shape[0] > 0:
-        gdal_datasets = list(gdal_data["data_set"].values)
-    else:
-        gdal_datasets = []
+    gdal_datasets = list(gdal_data["data_set"].values)
 
     # H5py for one-dimensional data sets
     if health == "good":
-    with h5py.File(file) as data_set:
-        keys = data_set.keys()
-        keys = [k for k in keys if k not in ["meta", "time_index"]]
-        scale_factors = {k: data_set[k].attrs["scale_factor"] for k in keys}
-        units = {k: data_set[k].attrs["units"] for k in keys}
-        keys = [k for k in keys if k not in gdal_datasets]
-        data_sets = {k: data_set[k][:] for k in keys}
-        meta = data_set["meta"][:]
+        with h5py.File(file) as data_set:
+            keys = data_set.keys()
+            keys = [k for k in keys if k not in ["meta", "time_index"]]
+            scale_factors = {k: data_set[k].attrs["scale_factor"] for k in keys}
+            units = {k: data_set[k].attrs["units"] for k in keys}
+            keys = [k for k in keys if k not in gdal_datasets]
+            data_sets = {k: data_set[k][:] for k in keys}
+            meta = data_set["meta"][:]
 
-    # Now we have to calculate these "manually" (lol)
-    stat_dicts = []
-    for ds in data_sets.keys():
-        values = data_sets[ds]
-        if ds in VARIABLE_CHECKS:
-            max_threshold = VARIABLE_CHECKS[ds][1]
-            min_threshold = VARIABLE_CHECKS[ds][0]
-        else:
-            max_threshold = np.nan
-            min_threshold = np.nan
-        minv = np.min(values)
-        maxv = np.max(values)
-        meanv = np.mean(values)
-        stdv = np.std(values)
-        stat_dict = {"file": file,
-                     "data_set": ds,
-                     "min": minv,
-                     "max": maxv,
-                     "mean": meanv,
-                     "std": stdv,
-                     "min_threshold": min_threshold,
-                     "max_threshold": max_threshold}
-        stat_dicts.append(stat_dict)
+        # Now we have to calculate these "manually" (lol)
+        stat_dicts = []
+        for ds in data_sets.keys():
+            values = data_sets[ds]
+            if ds in VARIABLE_CHECKS:
+                max_threshold = VARIABLE_CHECKS[ds][1]
+                min_threshold = VARIABLE_CHECKS[ds][0]
+            else:
+                max_threshold = np.nan
+                min_threshold = np.nan
+            minv = np.min(values)
+            maxv = np.max(values)
+            meanv = np.mean(values)
+            stdv = np.std(values)
+            stat_dict = {"file": file,
+                         "data_set": ds,
+                         "min": minv,
+                         "max": maxv,
+                         "mean": meanv,
+                         "std": stdv,
+                         "min_threshold": min_threshold,
+                         "max_threshold": max_threshold}
+            stat_dicts.append(stat_dict)
 
-    # Make another data frame with the 1-D data set statistics
-    hdf_data = pd.DataFrame(stat_dicts)
+        # Make another data frame with the 1-D data set statistics
+        hdf_data = pd.DataFrame(stat_dicts)
 
-    # Concatenate the two together if needed
-    if gdal_data.shape[0] > 0:
+        # Concatenate the two together if needed
         summary_df = pd.concat([gdal_data, hdf_data]).reset_index(drop=True)
-    else:
-        summary_df = hdf_data
 
-    # Add the scale factors and units
-    summary_df["scale_factor"] = summary_df["data_set"].map(scale_factors)
-    summary_df["units"] = summary_df["data_set"].map(units)
+        # Add the scale factors and units
+        summary_df["scale_factor"] = summary_df["data_set"].map(scale_factors)
+        summary_df["units"] = summary_df["data_set"].map(units)
 
-    # Now let's get a clue to our location
-    meta_df = pd.DataFrame(meta)
-    meta_df["reV_tech"] = meta_df["reV_tech"].apply(
-            lambda x: x.decode("utf-8"))
-
-
-    # Different meta data between wind and solar, no urban or pop in wind
-    if "wind" in meta_df["reV_tech"].unique():
-        meta_df["county"] = meta_df["county"].apply(lambda x: x.decode("utf-8"))
-        meta_df["country"] = meta_df["country"].apply(lambda x: x.decode("utf-8"))
-        meta_df["locale"] = meta_df["county"] +", " + meta_df["country"]
-        locale = meta_df["locale"][~pd.isnull(meta_df["county"])].iloc[0]
-        summary_df["sample_location"] = locale
-    else:
-        meta_df["urban"] = meta_df["urban"].apply(lambda x: x.decode("utf-8"))
-        meta_df["country"] = meta_df["country"].apply(
+        # Now let's get a clue to our location
+        meta_df = pd.DataFrame(meta)
+        meta_df["reV_tech"] = meta_df["reV_tech"].apply(
                 lambda x: x.decode("utf-8"))
-        max_pop = meta_df["population"].max()
-        city = meta_df[["urban", "country"]][meta_df["population"] == max_pop]
-        city = ", ".join(city.values[0])
-        summary_df["largest_city"] = city
 
-    # Lets also get overall min and max
-    group = summary_df.groupby("data_set")
-    summary_df["overall_min"] = group["min"].transform("min")
-    summary_df["overall_max"] = group["max"].transform("max")
+        # Different meta data between wind and solar, no urban or pop in wind
+        if "wind" in meta_df["reV_tech"].unique():
+            meta_df["county"] = meta_df["county"].apply(lambda x: x.decode("utf-8"))
+            meta_df["country"] = meta_df["country"].apply(lambda x: x.decode("utf-8"))
+            meta_df["locale"] = meta_df["county"] +", " + meta_df["country"]
+            locale = meta_df["locale"][~pd.isnull(meta_df["county"])].iloc[0]
+            summary_df["sample_location"] = locale
+        else:
+            meta_df["urban"] = meta_df["urban"].apply(lambda x: x.decode("utf-8"))
+            meta_df["country"] = meta_df["country"].apply(
+                    lambda x: x.decode("utf-8"))
+            max_pop = meta_df["population"].max()
+            city = meta_df[["urban", "country"]][meta_df["population"] == max_pop]
+            city = ", ".join(city.values[0])
+            summary_df["largest_city"] = city
 
-    # Finally, in case the file is broken, add a flag
-    summary_df["file_health"] = health
+        # Lets also get overall min and max
+        group = summary_df.groupby("data_set")
+        summary_df["overall_min"] = group["min"].transform("min")
+        summary_df["overall_max"] = group["max"].transform("max")
+
+        # Finally, in case the file is broken, add a flag
+        summary_df["file_health"] = health
+
+    else:
+        columns = ['file', 'data_set', 'min', 'max', 'mean', 'std', 'min_threshold', 'max_threshold', 'scale_factor',
+                   'units', 'largest_city', 'overall_min', 'overall_max', 'file_health']
+        values = {c: np.nan for c in columns}
+        summary_df = pd.DataFrame(values, index=[0])
+        summary_df["file"] = file
+        summary_df["file_health"] = health
 
     return summary_df
 
