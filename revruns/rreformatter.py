ee5d7d951d5580722bbb2c790d2a556c83bf7111
williamstravis <travis.williams@nrel.gov>
2022-06-29 19:36:33 -0600

    adding percent cover rasterization routine.

--- revruns/rreformatter.py
+++ revruns/rreformatter.py
@@ -24,12 +24,62 @@ import rasterio as rio
 from rasterio import features
 from pyproj import Transformer
 from revruns.rr import crs_match, isint, isfloat
+from shapely.geometry import box
 from tqdm import tqdm
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 NEEDED_FIELDS = ["name", "path"]
+
+
+def _rasterize_geom(geom, shape, atrans, all_touched):
+    indata = [(geom, 1)]
+    rv_array = features.rasterize(
+        indata,
+        out_shape=shape,
+        transform=atrans,
+        fill=0,
+        all_touched=all_touched)
+    return rv_array
+
+
+def rasterize_pctcover(geom, atrans, shape):
+    default = _rasterize_geom(geom, shape, atrans, all_touched=False)
+    alltouched = _rasterize_geom(geom, shape, atrans, all_touched=True)
+    exterior = _rasterize_geom(geom.exterior, shape, atrans, all_touched=True)
+
+    # Create percent cover grid as the difference between them
+    # at this point all cells are known 100% coverage,
+    # we'll update this array for exterior points
+    pctcover = (alltouched - exterior)
+
+    # loop through indicies of all exterior cells
+    for r, c in zip(*np.where(exterior == 1)):
+
+        # Find cell bounds, from rasterio DatasetReader.window_bounds
+        window = ((r, r+1), (c, c+1))
+        ((row_min, row_max), (col_min, col_max)) = window
+        x_min, y_min = (col_min, row_max) * atrans
+        x_max, y_max = (col_max, row_min) * atrans
+        bounds = (x_min, y_min, x_max, y_max)
+
+        # Construct shapely geometry of cell
+        cell = box(*bounds)
+
+        # Intersect with original shape
+        cell_overlap = cell.intersection(geom)
+
+        # update pctcover with percentage based on area proportion
+        coverage = cell_overlap.area / cell.area
+        pctcover[r, c] = int(coverage)
+
+    # Print out areas
+    print("{:,}sq. km".format((default.sum() * 90 * 90) / 1_000_000))
+    print("{:,}sq. km".format((alltouched.sum() * 90 * 90) / 1_000_000))
+    print("{:,}sq. km".format((pctcover.sum() * 90 * 90) / 1_000_000))
+
+    return pctcover
 
 
 class Exclusions:
@@ -599,3 +649,54 @@ class Reformatter(Exclusions):
             print(f"Building/updating exclusion {self.excl_fpath}...")
             self.to_h5()
 
+
+def test_partial():
+    """Test partial rasterization routines."""
+    import fiona
+    import matplotlib.pyplot as plt
+
+    from affine import Affine
+    from shapely.geometry import shape
+
+
+    df = gpd.read_file("/home/travis/data/shapefiles/tl_2021_us_state.shp")
+    df = df.to_crs("epsg:2857")
+    geom = df["geometry"][0]
+
+    xs = np.array(geom.exterior.xy[0])
+    ys = np.array(geom.exterior.xy[1])
+    xmin = xs.min()
+    ymin = ys.min()
+    xmax = xs.max()
+    ymax = ys.max()
+
+    res = 90
+
+    width = int(np.ceil((xmax - xmin) / res))
+    height = int(np.ceil((ymax - ymin) / res))
+
+    atrans = rio.transform.from_bounds(xmin, ymin, xmax, ymax, width, height)
+
+    shape = (height, width)
+
+    profile = {
+        'affine': atrans,
+        'height': shape[0],
+        'width': shape[1],
+        'count': 1,
+        'crs': "epsg:2857",
+        'driver': 'GTiff',
+        'dtype': 'uint8',
+        'nodata': None,
+        'tiled': False
+    }
+
+    pctcover = rasterize_pctcover(geom, atrans=atrans, shape=shape)
+    plt.imshow(pctcover)
+
+    with rio.open('/home/travis/scratch/percent_cover.tif', 'w', **profile) as dst:
+        dst.write(pctcover, 1)
+
+
+if __name__ == "__main__":
+    test_partial()
